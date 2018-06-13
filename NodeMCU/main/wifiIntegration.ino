f03e1cb104fa31bd63361e566afe4e2b86ddc532
kodeeey <kodeeey@gmail.com>
2018-06-13 11:17:54 +0200

    -Added WIFI Functions

---
+++ NodeMCU/main/wifiIntegration.ino
@@ -0,0 +1,21 @@
+#include <ESP8266WiFi.h>
+
+void wifiConnect(String ssid, String password)
+{
+  Serial.print("Start");
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
