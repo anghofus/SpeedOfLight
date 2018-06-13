7e370005bc608a6f7470f73912c1180cacefc35a
kodeeey <kodeeey@gmail.com>
2018-06-13 17:05:13 +0200

    -Added HTTP Server

--- NodeMCU/main/main.ino
+++ NodeMCU/main/main.ino
@@ -1,12 +1,46 @@
+#include <ESP8266WiFi.h>
+
+WiFiServer server(80);
+String header;
+  
 void setup() {
   initLed();
   initDisplay();
   delay(300);
-  
+  ledBlink(1000);
   wifiConnect("PiOfLight", "SchnellerVerkehr");
+  server.begin();
 }
 
 void loop() {
-  ledBlink(0, 500);
-  delay(300);
+  WiFiClient client = server.available();
+ 
+  if (client) {
+    updateText("New Client.", "");
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        header += c;
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+    header = "";
+    client.stop();
+ }
 }
