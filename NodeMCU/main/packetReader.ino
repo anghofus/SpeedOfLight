4a113c2426ef1e46f02db12dcb9570bd651a481e
kodeeey <kodeeey@gmail.com>
2018-06-14 16:17:44 +0200

    -Fixed Minor Bugs

--- NodeMCU/main/packetReader.ino
+++ NodeMCU/main/packetReader.ino
@@ -1,26 +1,15 @@
 String readFromHeader(String header) {
-  if (header.indexOf("setText=") >0) {
-    header.replace("GET /", "");
-    header = header.substring(0, header.indexOf(" HTTP/1.1"));
-
-    return header;
-
-  } else if (header.indexOf("setLight=") >0) {
-    header.replace("GET /", "");
-    header = header.substring(0, header.indexOf(" HTTP/1.1"));
-
-    return header;
-  }
-
-    header = "ERROR";
-    return header;
+  header.replace("GET /", "");
+  header = header.substring(0, header.indexOf(" HTTP/1.1"));
+  return header;
 }
 
+
 int getOutputType(String output) {
-  if (output.indexOf("setText=") >0) {
+  if (output.indexOf("setText=") == 0) {
 
     return 0;
-  } else if (output.indexOf("setLight=") >0) {
+  } else if (output.indexOf("setLight=") == 0) {
 
     return 1;
   }
