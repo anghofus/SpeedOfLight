1c5c99d3df4e923aa8235204f4db017f15cf370d
kodeeey <kodeeey@gmail.com>
2018-06-14 17:17:16 +0200

    -Updated For Two-Line Messages

--- NodeMCU/main/cases.ino
+++ NodeMCU/main/cases.ino
@@ -1,17 +1,34 @@
 int outputType;
+int linebreakPos;
+String displayLineOne;
+String displayLineTwo;
 
 void executeCase(String output) {
   outputType = getOutputType(output);
 
   switch (outputType) {
     case 0:
-    output.replace("setText=", "");
-    updateText(output, "");
+    if (output.indexOf("+") > 0) {
+      output.replace("setText=", "");
+      linebreakPos = output.indexOf("");
+      displayLineOne = output.substring(0, linebreakPos);
+      displayLineTwo = output.substring(linebreakPos, output.length());
+      updateText(displayLineOne, displayLineTwo);
+
+
+    } else {
+      output.replace("setText=", "");
+      updateText(output, "");
+    }
     break;
+
     case 1:
     output.replace("setLight=", "");
-    updateText(output, "");
+    if (output = "GREEN") {
+      //DO STUFF HERE
+    }
     break;
+
     default:
     updateText("ERROR", "");
     break;
